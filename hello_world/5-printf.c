a8d6fc68825b76f1594cae586178ceff63dad11a
kmilo2811V <5277@holbertonschool.com>
2022-06-13 18:36:38 -0700

    command puts

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+   /**
+    *main-impresin
+    *
+    *Return: retorna 0
+    */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
