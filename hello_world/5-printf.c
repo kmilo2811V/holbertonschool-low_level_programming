ca5cf9fdba867d675bc4eee09b0579d31afcc70d
kmilo2811V <5277@holbertonschool.com>
2022-06-13 18:57:29 -0700

    command puts

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-   /**
-    *main - impresion of prueba
-    *
-    *Return: retorna 0
-    */
+/**
+*main - compilar
+*
+*Return: retorna 0
+*/
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
