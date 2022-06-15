aee2e71fbf154b8009ed5fb9775ebbb9056a47e7
kmilo2811V <5277@holbertonschool.com>
2022-06-14 19:58:00 -0700

    the last digil

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - this program will assign a random number to the variable n each timeis executed*/
-*Return 0
+ * main - the function with n and arithmetical conditions
+ * Return: 0
+ */
 int main(void)
-*/
-  int main(void);
 {
-  int, n, ld;
+  int n, ld;
 
   srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    ld = n% 10;
-    if (ld > 5)
-      printf("last digit of %d is %d and is greater than 5\n", n, ld);
-    else if (ld == 0)
-      printf("last digit of %d id %d and is 0\n", n, ld);
-    else if (ld < 6 && ld != 0)
-      printf("last digit of %d is %d and is less than 6 and not 0\n" n, ld);
-    return (0);
+  n = rand() - RAND_MAX / 2;
+  ld = n % 10;
+  if (ld > 5)
+    printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+  else if (ld == 0)
+    printf("Last digit of %d is %d and is 0\n", n, ld);
+  else if (ld < 6 && ld != 0)
+    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+  return (0);
 }
