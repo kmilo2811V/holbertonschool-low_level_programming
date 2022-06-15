6c0a68cfcc0b379045baa6855cfc3df903e439f8
kmilo2811V <5277@holbertonschool.com>
2022-06-15 16:35:26 -0700

    the vocabulary of the 0 and 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - the function print the numbers 0-1
+*
+*Return: 0
+*/
+
+{
+int main(void)
+{
+int i=0;
+while(i<10)
+printf("%d",i);
+i++;
+}
+}
