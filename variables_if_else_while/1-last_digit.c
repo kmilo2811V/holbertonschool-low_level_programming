84027c0de9ce633802b27b27c5247f4420263bf4
kmilo2811V <5277@holbertonschool.com>
2022-06-14 19:41:15 -0700

    command teh las diggit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* This program will assign a random number to the variable n each time it is execut\
-   ed.*/
-*
+/**
+*main - this program will assign a random number to the variable n each timeis executed*/
+*Return 0
 int main(void)
+*/
+  int main(void);
 {
-  int n;
+  int, n, ld;
 
-  srand(time(0))
+  srand(time(0));
     n = rand() - RAND_MAX / 2;
-  RETURN (0);
+    ld = n% 10;
+    if (ld > 5)
+      printf("last digit of %d is %d and is greater than 5\n", n, ld);
+    else if (ld == 0)
+      printf("last digit of %d id %d and is 0\n", n, ld);
+    else if (ld < 6 && ld != 0)
+      printf("last digit of %d is %d and is less than 6 and not 0\n" n, ld);
+    return (0);
 }
