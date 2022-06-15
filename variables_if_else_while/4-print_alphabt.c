dc704aa8e8fa3d4c9f4d5d2b7ed5816ef47eaef8
kmilo2811V <5277@holbertonschool.com>
2022-06-15 14:25:12 -0700

    vocabulary excep q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - printf vocavulary excep q and e
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int abc = 'a';
+while (abc <= 'd')
+while (abc <= 'p')
+{
+putchar(abc);
+abc++;
+}
+putchar('\n');
+return (0);
+}
