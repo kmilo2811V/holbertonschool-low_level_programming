4221fec449636e4fe46f2e517a0f9b431add3434
kmilo2811V <5277@holbertonschool.com>
2022-06-14 19:25:47 -0700

    command teh las diggit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* This program will assign a random number to the variable n each time it is execut\
+   ed.*/
+*
+int main(void)
+{
+  int n;
+
+  srand(time(0))
+    n = rand() - RAND_MAX / 2;
+  RETURN (0);
+}
