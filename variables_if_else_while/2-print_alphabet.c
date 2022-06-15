fb4f8e686da098d324c04a07bb804f81b1ba9522
kmilo2811V <5277@holbertonschool.com>
2022-06-15 12:33:39 -0700

    vocabulario

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+  char abc = 'a';
+  while (abc <= 'z' )
+    {
+      putchar(abc);
+      abc++;
+    }
+  return (0);
+}
