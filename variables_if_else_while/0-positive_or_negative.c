6bbc18d44dc88020961fa7b00c72475cb015542c
kmilo2811V <5277@holbertonschool.com>
2022-06-14 17:42:19 -0700

    command if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - the function find the positive, negativo o zero of 
-* Return: 0
-*/
+ * main - the function find the positive, negativo o zero of
+ * Return: 0
+ */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n > 0)
-printf("%d is positive\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n == 0)
+    printf("%d is zero\n", n);
+  else if (n > 0)
+    printf("%d is positive\n", n);
+  else
+    printf("%d is negative\n", n);
+  return (0);
 }
