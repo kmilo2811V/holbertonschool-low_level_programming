68f5272384b60f2bc99c87b958366b71d51bae39
kmilo2811V <5277@holbertonschool.com>
2022-06-15 16:57:59 -0700

    the number 0-9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - the function print the numbers 0-1
-*
-*Return: 0
-*/
+ *main - the function print the numbers 0-1
+ *
+ *Return: return zero
+ */
 
-{
 int main(void)
 {
-int i=0;
-while(i<10)
-printf("%d",i);
-i++;
-}
+int i;
+for(i=0; i<10; i++)
+printf("%d ",i);
+return (0);
 }
