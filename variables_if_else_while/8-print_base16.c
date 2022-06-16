2d7510485afc58e3c4c7006f9114927da6b7f785
kmilo2811V <5277@holbertonschool.com>
2022-06-15 22:17:48 -0700

    print vocabulary

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print the vocabulary
+*
+* Return zero
+*/
+
+int main(void)
+{
+int abc = 'a';
+int i;
+for (i = 48; i <= 57; i++)
+putchar (i);
+while (abc <= 'f')
+{
+putchar(abc);
+abc++;
+}
+putchar('\n');
+return (0);
+}
