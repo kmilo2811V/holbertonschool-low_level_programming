33cd8e952b07329b5ae229359cafcd1d9f46798d
kmilo2811V <5277@holbertonschool.com>
2022-06-16 18:31:44 -0700

    Write a program that prints _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - Write a program that prints _putchar
+ *
+ * Return: 0
+ */
+int main()
+{
+  _putchar(95);
+  _putchar(112);
+  _putchar(117);
+  _putchar(116);
+  _putchar(99);
+  _putchar(104);
+  _putchar(97);
+  _putchar(114);
+  _putchar(10);
+  return (0);
+}
+    
