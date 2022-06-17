983c70be9350f984d2de3047d0d498af675c336e
kmilo2811V <5277@holbertonschool.com>
2022-06-16 18:38:33 -0700

    Write a program that prints _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
  *
  * Return: 0
  */
-int main()
+int main(void)
 {
-  _putchar (95);
-  _putchar (112);
-  _putchar (117);
-  _putchar (116);
-  _putchar (99);
-  _putchar (104);
-  _putchar (97);
-  _putchar (114);
-  _putchar (10);
-  return (0);
+_putchar (95);
+_putchar (112);
+_putchar (117);
+_putchar (116);
+_putchar (99);
+_putchar (104);
+_putchar (97);
+_putchar (114);
+_putchar (10);
+return (0);
 }
