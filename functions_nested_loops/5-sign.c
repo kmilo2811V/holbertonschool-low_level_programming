86712c47a7af86f59e6df0cde42558332a12bfc9
kmilo2811V <5277@holbertonschool.com>
2022-06-19 22:17:20 -0700

    Write a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - print the 1 when is mayus or 0 when is lower
+ *@n: this is variable of alphabet
+ *Return: retorna 1 o 0 si no
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);                 
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
