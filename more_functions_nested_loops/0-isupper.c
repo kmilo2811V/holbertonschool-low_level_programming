323f288677f026466b8edce4e6d4c69c69f0760b
kmilo2811V <5277@holbertonschool.com>
2022-06-23 21:20:41 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - check the caracter
+*@c:imput
+*Return: 1 if c is uppercasa, 0 if not
+*/
+
+int _isupper(int c)
+
+{
+if ((c >= 'A') && (c <= 'Z'))
+
+return (1);
+else
+
+return (0);
+}
