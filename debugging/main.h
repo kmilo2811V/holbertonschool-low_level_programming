be0bc1131f19304afcf3af3608eb535e10195765
kmilo2811V <5277@holbertonschool.com>
2022-06-23 17:37:27 -0700

    Leap year

--- debugging/main.h
+++ debugging/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,3 +23,4 @@ int add(int, int);
 void print_to_98(int n);
 void positive_or_negative(void)
 int largest_number(int a, int b, int c)
+int print_remaining_days(void)
