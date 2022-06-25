962a4600b352510a42fc8a0e7548584ad14bd6b0
kmilo2811V <5277@holbertonschool.com>
2022-06-25 16:42:40 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - code print number 1 to 100.
+*Return: Always 0.
+*/
+int main(void)
+{
+int i;
+for (i = 1; i < 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else
+printf("%i ", i);
+}
+printf("Buzz\n");
+return (0);
+}
